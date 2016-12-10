7ebf6751eeb753197ebd9863b2f00a0a14ac4d98
Martin Sova <martinkevinsova@gmail.com>
2016-12-10 01:59:43 +0000

    work in progress

--- difficultyfunctions.js
+++ difficultyfunctions.js
@@ -1,3 +1,5 @@
+//remember to color indicate the buttons
+
 if counter (counter === 1) {
 function firstWrong(question) {
 	var new_string = question;
@@ -15,6 +17,28 @@ function firstWrong(question) {
 }
 
 else if (counter === 2) {
-	
-	
+	function secondWrong(question) {
+		//swap true and false (right answer is wrong and wrong answer is right)
+	}
+else if (counter === 3) {
+
+	function thirdWrong(question) {
+	var new_string = question;
+	for (var i = 0 ; i < new_string.length; i++) {
+		if (i % 2 = 0) {
+			new_string[]
+
+		}
+		i += 2;
+		var second = new_string[i];
+		var first = new_string[i-2];
+		new_string[i-2] = second;
+		new_string[i] = first;
+	}
+	return new_string;
+
+	}
+
+
 }
+	
